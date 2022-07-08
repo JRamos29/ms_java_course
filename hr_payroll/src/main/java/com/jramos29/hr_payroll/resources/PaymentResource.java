ca16dff9b4b9ce990128ea84168fad2dff35728c
JRamos29 <malebolge@gmail.com>
2022-07-08 15:08:29 -0300

    Implemented fault tolerance with Resilience4j and Actuator;

--- hr_payroll/src/main/java/com/jramos29/hr_payroll/resources/PaymentResource.java
+++ hr_payroll/src/main/java/com/jramos29/hr_payroll/resources/PaymentResource.java
@@ -10,17 +10,26 @@ import org.springframework.web.bind.annotation.RestController;
 import com.jramos29.hr_payroll.entities.Payment;
 import com.jramos29.hr_payroll.services.PaymentService;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+
 @RestController
 @RequestMapping(value = "/payments")
 public class PaymentResource {
+
+    private static final String HR_WORKER = "hr-worker";
 
     @Autowired
     private PaymentService service;
 
     @GetMapping(value = "/{workerId}/days/{days}")
+    @CircuitBreaker(name = HR_WORKER, fallbackMethod = "getPaymentFallback")
     public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days) {
         Payment payment = service.getPayment(workerId, days);
 
         return ResponseEntity.ok(payment);
+    }
+
+    public ResponseEntity<String> getPaymentFallback(Exception e) {
+        return ResponseEntity.ok("hr-worker service is down: " + e.getMessage());
     }
 }
