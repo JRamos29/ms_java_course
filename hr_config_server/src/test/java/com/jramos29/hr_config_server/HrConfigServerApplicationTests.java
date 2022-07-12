9d6bcb72e1823deac6ef8edc97694b9341559d87
JRamos29 <malebolge@gmail.com>
2022-07-12 17:28:19 -0300

    Project hr_config_server created;

---
+++ hr_config_server/src/test/java/com/jramos29/hr_config_server/HrConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jramos29.hr_config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
