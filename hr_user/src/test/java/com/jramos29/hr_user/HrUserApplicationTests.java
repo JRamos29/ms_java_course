0b4f8856e9253e0ed5280267a5e7ef6bd7622d20
JRamos29 <malebolge@gmail.com>
2022-07-14 15:36:02 -0300

    Project hr_user created;

---
+++ hr_user/src/test/java/com/jramos29/hr_user/HrUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jramos29.hr_user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
